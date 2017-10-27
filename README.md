924a67296032cb3db49037b56bc7a3fc4778d93d
Charles Sam Dilip <j.samdilip@gmail.com>
2017-10-27 23:43:58 +0530

    Protobuf complete

--- README.md
+++ README.md
@@ -5,11 +5,14 @@
 * Kafka [0.11.0.1](https://www.apache.org/dyn/closer.cgi?path=/kafka/0.11.0.1/kafka-0.11.0.1-src.tgz)
 * Kafka-clients [0.11.0.1](http://central.maven.org/maven2/org/apache/kafka/kafka-clients/0.11.0.1/kafka-clients-0.11.0.1.jar)
 * protobuf [3.4.0](https://github.com/google/protobuf/releases/tag/v3.4.0) (Compiler for the proto template)
-* protobuf-java [3.4.0](https://mvnrepository.com/artifact/com.google.protobuf/protobuf-java/3.4.0) 
-
+* protobuf-java [3.4.0](http://central.maven.org/maven2/com/google/protobuf/protobuf-java/3.4.0/protobuf-java-3.4.0.jar)
+* protobuf-util [3.4.0](http://central.maven.org/maven2/com/google/protobuf/protobuf-java-util/3.4.0/protobuf-java-util-3.4.0.jar)
+* gson [2.8.2](http://central.maven.org/maven2/com/google/code/gson/gson/2.8.2/gson-2.8.2.jar)
 
 ## Build
 * Build packages of protobuf in "com.json_push.template" into project src
+- Move to the project directory (netty-json_push)
+- execute `protoc --java_out=src template/proto/query.proto`
 * Build the project with jars of Netty, Kafka-clients, protobuff
 
 ## Start the server
--- netty-json_push/src/com/json_push/helper/ProtoBufHelper.java
+++ netty-json_push/src/com/json_push/helper/ProtoBufHelper.java
@@ -1,28 +1,41 @@
 package com.json_push.helper;
 
-import com.json_push.protobuf.QueryProto.Query;
+import java.util.List;
+import java.util.Map;
 
-import java.net.URI;
+import com.json_push.protobuf.QueryProtobuf.ParamValue;
+import com.json_push.protobuf.QueryProtobuf.Query;
+import com.google.protobuf.util.JsonFormat;
+
+// import io.netty.handler.codec.http.QueryStringDecoder;
 
 public class ProtoBufHelper {
 	
-	
-	public static Query queryToProtobuf(String queryStr) {
+	public static Query queryToProtobuf(Map<String, List<String>> parameterMap) {
 		Query.Builder queryBld = Query.newBuilder();
-		if (!queryStr.equals("")) {
-			String[] queries = queryStr.split("&");
-			for (String query:queries) {
-				String[] paramPair = query.split("=");
-				queryBld.putPairs(paramPair[0], paramPair.length == 2 ? paramPair[1] : "");
+		
+		for (String key: parameterMap.keySet()) {
+			List<String> values = parameterMap.get(key);
+			ParamValue.Builder paramBld = ParamValue.newBuilder();
+			
+			for (String value:values) {
+				paramBld.addValue(value);
 			}
+			queryBld.putQueryParams(key, paramBld.build());
 		}
+		
 		return queryBld.build();
+	}
+	
+	public static String protobufToJson(Query query) throws Exception {
+		JsonFormat.Printer printer = JsonFormat.printer();
+		return printer.print(query);
 	}
 
 	/*public static void main(String[] args) throws Exception {
-		URI uri = new URI("/test?a=1");
-		Query query = queryToProtobuf(uri.getQuery());
-		System.out.println(query.toString());
+		QueryStringDecoder decoder = new QueryStringDecoder("/test?a=1&b=2&a=3");
+		Query query = queryToProtobuf(decoder.parameters());
+		System.out.println(protobufToJson(query));
 	}*/
 	
 }
